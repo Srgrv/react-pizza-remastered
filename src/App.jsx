a67508bc65312b74a572ae2584a3afd1c229ff40
Sergey Georgiev <sgrgv_art@mail.ru>
2023-10-12 14:38:52 +0300

    добавил диначеский параметр

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { useSearchParams } from "react-router-dom";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
+import FullPizza from "./pages/FullPizza";
 
 //components
 
@@ -29,6 +30,7 @@ function App() {
           }
         />
         <Route path="cart" element={<Cart />} />
+        <Route path="pizza/:id" element={<FullPizza />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
